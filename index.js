5595e5afb8fe744eb441c09663e8cb67058a2935
Mie O <miei1@outlook.com>
2017-04-22 13:51:18 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function BrokerAnywhere(dispatch){
+    const chatHook = event => {
+        if(!event.message.includes('!broker')) return
+        
+        dispatch.toClient('S_NPC_MENU_SELECT', 1, {type:28})
+        return false
+    }
+    dispatch.hook('C_CHAT', 1, chatHook)
+    dispatch.hook('C_WHISPER', 1, chatHook)
+}
